216b57834dbb23ca174feeb28a2d88bc2b1f16fd
Xiphe <github@xiphe.net>
2016-06-21 23:51:17 +0200

    feat: make configurable

--- index.js
+++ index.js
@@ -3,31 +3,45 @@
 const meta = require('./package');
 const path = require('path');
 const webpack = require('webpack');
+const webpackConfig = require('uberconfig-webpack-common');
 const WebpackDevServer = require('webpack-dev-server');
+const logger = require('gulplog');
 
 module.exports = {
   meta,
-  config: {},
-  get() {
+  config: Object.assign({}, webpackConfig.defaults, {
+    'webpack.entry.demoApp': {
+      default: './demo/app.js',
+    },
+    'demo.hot': { default: true },
+    'demo.folder': { default: 'demo/' },
+    'demo.port': { default: 8000 },
+    'demo.host': { default: 'localhost' },
+  }),
+  get(helper) {
     return function demo() {
-      const compiler = webpack({
-        devtool: 'eval',
-        debug: false,
-        entry: {
-          demoApp: path.resolve('demo/app.js'),
-        },
-        output: {
-          path: path.resolve('dist/'),
-          filename: 'demoApp.js',
-          publicPath: '/',
-        },
-      });
+      const config = helper.getConfig();
+      let webpackDemoConfig = webpackConfig(config);
 
+      if (!config.production && config.demo.hot) {
+        webpackDemoConfig = webpackDemoConfig.merge({
+          plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+          ],
+        });
+        webpackDemoConfig.entry.demoApp = [].concat(
+          webpackDemoConfig.entry.demoApp,
+          `webpack-dev-server/client?http://${config.demo.host}:${config.demo.port}/`
+        );
+      }
+
+      const compiler = webpack(webpackDemoConfig);
       const server = new WebpackDevServer(compiler, {
-        contentBase: path.resolve('demo/'),
+        contentBase: path.resolve(config.demo.folder),
       });
 
-      server.listen(8000, 'localhost');
+      server.listen(config.demo.port, config.demo.host);
+      logger.info(`demo running: http://${config.demo.host}:${config.demo.port}`);
     };
   },
 };
